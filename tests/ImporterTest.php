d7abf6e200eb9376dff3a9e457459301efa84944
Brad Jones <brad@bjc.id.au>
2016-02-23 12:46:20 +1100

    Added tests

---
+++ tests/ImporterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+Brads\Importer::globalise();
+
+class ImporterTest extends PHPUnit_Framework_TestCase
+{
+    public function testImportWithoutScope()
+    {
+        $this->assertEquals([], import(__DIR__.'/import-test-file.php'));
+    }
+
+    public function testImportWithScope()
+    {
+        $scope = ['foo' => 'bar'];
+        $this->assertEquals($scope, import(__DIR__.'/import-test-file.php', $scope));
+    }
+
+    public function testImportInclude()
+    {
+        $this->setExpectedExceptionRegExp
+        (
+            'PHPUnit_Framework_Error_Warning',
+            '/^include\(not_existing_file\.php\)/'
+        );
+
+        import('not_existing_file.php', null, false);
+    }
+}
